db06dd37192ed0ccd712a2596fc8260c85720112
arvin sadeghi <arving2079@gmail.com>
2020-05-06 18:39:16 +0430

    inital color pallet and avatar created

---
+++ application/planb/lib/src/ui/constants/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+//color pallet
+final Color primaryColor = Color(0xFF1A237E);
+final Color secondaryColor = Color(0xFFb71c1c);
+final Color backgroundColor = Color(0xFFeeeeee);
+final Color button1Color = Color(0xFF3A9BBF);
+final Color dangerColor = Color(0xFFCD1C1C);
+final Color green1Color = Color(0xFF05A719);
+final Color green2Color = Color(0xFFCD1C1C);
+
+
+
+
