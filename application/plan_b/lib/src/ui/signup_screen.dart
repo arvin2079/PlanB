f967bb90a8f3735c7c9fd6def21c06e7f68ebf1b
arvin sadeghi <arving2079@gmail.com>
2020-05-08 15:39:45 +0430

    autoCompleteSearch TextField and itemExist snackbar fixed

--- application/plan_b/lib/src/ui/signup_screen.dart
+++ application/plan_b/lib/src/ui/signup_screen.dart
@@ -13,6 +13,7 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   TextEditingController _searchInputController = TextEditingController();
+
   List<String> sexItems = <String>['پسر', 'دختر'];
 
   List<String> _uniItems = <String>[
@@ -56,168 +57,181 @@ class _SignupScreenState extends State<SignupScreen> {
             ),
           ],
         ),
-        body: LimitedBox(
-          maxHeight: double.maxFinite,
-          maxWidth: double.maxFinite,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Avatar(
-                        icon: Icons.add,
-                        iconSize: 30,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('نام'),
-                          Text('نام خوانوادگی'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  CustomtextField(labeltext: 'نام دانشگاه'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      DropdownButton(
-                        hint: Text('جنسیت'),
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        items: sexItems.map((value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                      DropdownButton(
-                        hint: Text('دانشگاه'),
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        items: _uniItems.map((value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    'اطلاعات تماس',
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.right,
-                  ),
-                  CustomtextField(
-                    labeltext: 'موبایل',
-                    inputType: TextInputType.phone,
-                  ),
-                  CustomtextField(
-                    labeltext: 'ایمیل',
-                    inputType: TextInputType.emailAddress,
-                  ),
-                  CustomtextField(
-                    labeltext: 'وبسایت',
-                    inputType: TextInputType.url,
-                  ),
-                  CustomtextField(labeltext: 'اینستاگرام'),
-                  CustomtextField(labeltext: 'تلگرام'),
-                  CustomtextField(labeltext: 'گیت'),
-                  CustomtextField(labeltext: 'لینکدین'),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    'اطلاعات تماس',
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.right,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: TextField(
-                      textDirection: TextDirection.rtl,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 5,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(2.0),
+        body: Builder(
+          builder: (context) => LimitedBox(
+            maxHeight: double.maxFinite,
+            maxWidth: double.maxFinite,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Avatar(
+                          icon: Icons.add,
+                          iconSize: 30,
                         ),
-                        labelText: 'خلاصه ای از سوابغ خود بنویسید',
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('نام'),
+                            Text('نام خوانوادگی'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    CustomtextField(labeltext: 'نام دانشگاه'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        DropdownButton(
+                          hint: Text('جنسیت'),
+                          onChanged: (value) {
+                            print(value);
+                          },
+                          items: sexItems.map((value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        DropdownButton(
+                          hint: Text('دانشگاه'),
+                          onChanged: (value) {
+                            print(value);
+                          },
+                          items: _uniItems.map((value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      'اطلاعات تماس',
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.right,
+                    ),
+                    CustomtextField(
+                      labeltext: 'موبایل',
+                      inputType: TextInputType.phone,
+                    ),
+                    CustomtextField(
+                      labeltext: 'ایمیل',
+                      inputType: TextInputType.emailAddress,
+                    ),
+                    CustomtextField(
+                      labeltext: 'وبسایت',
+                      inputType: TextInputType.url,
+                    ),
+                    CustomtextField(labeltext: 'اینستاگرام'),
+                    CustomtextField(labeltext: 'تلگرام'),
+                    CustomtextField(labeltext: 'گیت'),
+                    CustomtextField(labeltext: 'لینکدین'),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      'اطلاعات تماس',
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.right,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: TextField(
+                        textDirection: TextDirection.rtl,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(2.0),
+                          ),
+                          labelText: 'خلاصه ای از سوابغ خود بنویسید',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    'مهارت های شما',
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.right,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
-                    child: AutoCompleteTextField<String>(
-                      controller: _searchInputController,
-                      itemSubmitted: (data) {
-                        setState(() {
-                          if(!_chipsData.contains(data))
-                            _chipsData.add(data);
-                          else {
-                            // fixme : this part throw an error when i want to show alert snackbar
-//                            Scaffold.of(context).showSnackBar(SnackBar(
-//                              content: Text('این مهارت قبلا اضافه شده'),
-//                              action: SnackBarAction(
-//                                label: 'باشه',
-//                              ),
-//                            ));
-                          }
-                        });
-                      },
-                      suggestions: _getSearchFieldSuggestion(
-                          _searchInputController.text),
-                      key: GlobalKey(),
-                      itemFilter: (String suggestion, String query) {
-                        // FIXME : implement itemFilter for search
-                        return true;
-                      },
-                      itemSorter: (String a, String b) {
-                        // FIXME : implement comparator for search
-                        return 1;
-                      },
-                      itemBuilder: (BuildContext context, String suggestion) {
-                        // FIXME : make style for list item Texts
-                        return Text(suggestion);
-                      },
+                    SizedBox(
+                      height: 25,
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5),
-                    child: Wrap(
-                      children: _chipWidgets.toList(),
-                      spacing: 10.0,
+                    Text(
+                      'مهارت های شما',
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.right,
                     ),
-                  ),
-                  SizedBox(
-                    height: 200,
-                  ),
-                  RaisedButton(
-                    child: Text('ادامه و تکمیل حساب'),
-                    onPressed: () {},
-                  )
-                ],
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      child: AutoCompleteTextField<String>(
+                        controller: _searchInputController,
+                        itemSubmitted: (data) {
+                          setState(() {
+                            if(!_chipsData.contains(data))
+                              _chipsData.add(data);
+                            else {
+
+                              // fixme : this part throw an error when i want to show alert snackbar (fixed)
+                              // solution : This exception happens because you are using the context of the widget that instantiated Scaffold. Not the context of a child of Scaffold.
+                              // for this we add builder to the Scaffold body so builder parent is scaffold and can be userd via ther context.
+                              // solution : using globaleKey for the scaffold :
+                              // _scaffoldKey.currentState.showSnackBar(snackbar);
+
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text('این مهارت قبلا اضافه شده'),
+                                action: SnackBarAction(
+                                  label: 'باشه',
+                                  onPressed: (){},
+                                ),
+                              ));
+                            }
+                          });
+                        },
+                        suggestions: _getSearchFieldSuggestion(
+                            _searchInputController.text),
+                        key: GlobalKey(),
+                        itemFilter: (String suggestion, String query) {
+                          // FIXME : implement itemFilter for search (fixed)
+                          // using RegExp
+                          return suggestion.contains(RegExp(r'\b' + '${query.toLowerCase()}'));
+                        },
+                        itemSorter: (String a, String b) {
+                          // FIXME : implement comparator for search
+                          if(a.length < b.length)
+                            return -1;
+                          else
+                            return 1;
+                        },
+                        itemBuilder: (BuildContext context, String suggestion) {
+                          // FIXME : make style for list item Texts
+                          return Text(suggestion);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Wrap(
+                        children: _chipWidgets.toList(),
+                        spacing: 10.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200,
+                    ),
+                    RaisedButton(
+                      child: Text('ادامه و تکمیل حساب'),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -230,7 +244,12 @@ class _SignupScreenState extends State<SignupScreen> {
     return <String>[
       'hello',
       'this is apple',
-      'fuck',
+      'android',
+      'ios',
+      'art',
+      'python',
+      'front-end',
+      'back-end',
       'yellow',
       'arvin',
       'container',
@@ -240,10 +259,12 @@ class _SignupScreenState extends State<SignupScreen> {
 }
 
 class CustomtextField extends StatelessWidget {
-  const CustomtextField({this.inputType, @required this.labeltext});
+  const CustomtextField({this.focusNode, this.controller, this.inputType, @required this.labeltext});
 
   final String labeltext;
   final TextInputType inputType;
+  final FocusNode focusNode;
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
