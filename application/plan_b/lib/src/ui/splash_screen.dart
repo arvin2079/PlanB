c4938e30c9f5e6cb2f295816cb536713d9b1e1b4
Erfan Sobhaei <e.sobhaei@gmail.com>
2020-05-12 18:26:37 +0430

    SplashScreen created for viewing screens

--- application/plan_b/lib/src/ui/splash_screen.dart
+++ application/plan_b/lib/src/ui/splash_screen.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:planb/src/ui/home_screen.dart';
+import 'package:planb/src/ui/login_screen.dart';
+import 'package:planb/src/ui/signup_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+              child: Text("Signup"),
+              onPressed: (){
+                Navigator.push(context,
+                MaterialPageRoute(
+                  builder: (context) => SignupScreen()
+                ));
+              },
+            ),
+            FlatButton(
+              child: Text("Home"),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) => HomeScreen()
+                    ));
+              },
+            ),
+            FlatButton(
+              child: Text("Login"),
+              onPressed: (){
+                Navigator.push(context,
+                    MaterialPageRoute(
+                        builder: (context) => LoginScreen()
+                    ));
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
